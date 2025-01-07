7e0e36cf3f688267205d8995076b97439840ea73
Markiian2021 <165946925+Markiian2021@users.noreply.github.com>
2025-01-07 19:23:13 +0200

    Delete MuseumSenkiv directory

--- MuseumSenkiv/app/src/main/java/com/example/museumsenkiv/data/entity/response/departmentsResponse/DepartmentData.kt
+++
@@ -1,5 +0,0 @@
-package com.example.museumsenkiv.data.entity.response.departmentsResponse
-
-data class DepartmentData(
-    val departments: ArrayList<Department>
-)
--- MuseumSenkiv/app/src/main/java/com/example/museumsenkiv/data/entity/response/departmentsResponse/DepartmentIDs.kt
+++
@@ -1,7 +0,0 @@
-package com.example.museumsenkiv.data.entity.response.departmentsResponse
-
-
-data class DepartmentIDs(
-    val objectIDs: List<Int>,
-    val total: Int,
-)
